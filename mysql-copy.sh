ecf09780288c88282a5b316ebbec8efbce2b882a
Bastian Rang <justb81@users.noreply.github.com>
2019-03-17 12:37:04 +0100

    Update mysql-copy.sh

--- mysql-copy.sh
+++ mysql-copy.sh
@@ -11,7 +11,7 @@ echo "Destination user: ${MYSQL_DESTINATION_USER}"
 echo "Destination dbname: ${MYSQL_DESTINATION_DBNAME}"
 
 echo "dumping source database ..."
-mysqldump -h ${MYSQL_SOURCE_HOST} -u ${MYSQL_SOURCE_USER} -P ${MYSQL_SOURCE_PORT} -p${MYSQL_SOURCE_PASSWORD} ${MYSQL_SOURCE_DBNAME} -R --extended-insert --quick --single-transaction --max_allowed_packet=500M -f > temp.sql
+mysqldump -h ${MYSQL_SOURCE_HOST} -u ${MYSQL_SOURCE_USER} -P ${MYSQL_SOURCE_PORT} -p${MYSQL_SOURCE_PASSWORD} ${MYSQL_SOURCE_DBNAME} --single-transaction > temp.sql
 echo "creating destination database ..."
 mysql -h ${MYSQL_DESTINATION_HOST} -u ${MYSQL_DESTINATION_USER} -P ${MYSQL_DESTINATION_PORT} -p${MYSQL_DESTINATION_PASSWORD} -e "drop database ${MYSQL_DESTINATION_DBNAME}"
 mysql -h ${MYSQL_DESTINATION_HOST} -u ${MYSQL_DESTINATION_USER} -P ${MYSQL_DESTINATION_PORT} -p${MYSQL_DESTINATION_PASSWORD} -e "create database ${MYSQL_DESTINATION_DBNAME}"
